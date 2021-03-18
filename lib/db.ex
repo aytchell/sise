ab03bd6e31d69b4b61a1ef3e71940d6a5dfd7ffc
Hannes Lerchl <hannes.lerchl@googlemail.com>
2021-03-18 12:23:22 +0100

    First tiny steps for a device/service database

---
+++ lib/db.ex
@@ -0,0 +1,21 @@
+defmodule DB do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def add(pid, packet) do
+    GenServer.cast(pid, {:add, packet})
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast({:add, packet}, state) do
+    {:noreply, [packet | state]}
+  end
+end
