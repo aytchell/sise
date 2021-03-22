08fc1b06516643746246d4959c3b3f2e53ff2e00
Hannes Lerchl <hannes.lerchl@googlemail.com>
2021-03-22 16:41:50 +0100

    Implemented sending M-Search
    (on startup and every 20 minutes)

---
+++ lib/search/broadcaster.ex
@@ -0,0 +1,113 @@
+defmodule Ssdp.Search.Broadcaster do
+# SPDX-License-Identifier: Apache-2.0
+
+  require Logger
+
+  defmodule State do
+    defstruct [ :socket, :timer_ref, :attempts_left ]
+  end
+
+  @twenty_minutes 20 * 60 * 1000
+
+  use GenServer
+
+  def start_link(opts) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, %State{}, opts)
+    Logger.debug("Started M-SEARCH broadcaster")
+    GenServer.cast(pid, :ok)
+    {:ok, pid}
+  end
+
+  def stop(pid) do
+    GenServer.stop(pid)
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_cast(:ok, state) do
+    if is_nil(state.socket) do
+      Logger.debug("sending first M-SEARCH msg")
+      handle_first_msg()
+    else
+      Logger.debug("sending follow-up M-SEARCH msg")
+      handle_follow_up_msg(state)
+    end
+  end
+
+  @impl true
+  def handle_info(:ok, state) do
+    Logger.debug("handle_info")
+    GenServer.cast(self(), :ok)
+    {:noreply, %State{state | timer_ref: nil}}
+  end
+
+  def handle_info({:udp, _socket, _host, _port, _msg}, state) do
+    Logger.debug("received M-SEARCH answer")
+    {:noreply, state}
+  end
+
+  def handle_info(:timeout, state) do
+    Logger.debug("timeout -> closing socket")
+    close_socket(state)
+    {:noreply, %State{state | socket: nil}}
+  end
+
+  @impl true
+  def terminate(_reason, state) do
+    unless is_nil(state.timer_ref) do
+      Process.cancel_timer(state.timer_ref)
+    end
+    close_socket(state)
+    # the return value is ignored
+  end
+
+  # Open the socket and send the first M-SEARCH message
+  defp handle_first_msg() do
+    {:ok, socket} = open_unicast_socket()
+    send_msearch_message(socket)
+    {:noreply, %State{socket: socket, attempts_left: 2,
+      timer_ref: Process.send_after(self(), :ok, 1000)}}
+  end
+
+  # Send some more M-SEARCH messages in case the first message got lost
+  defp handle_follow_up_msg(state) do
+    if state.attempts_left == 0 do
+      # We're done. Wait some seconds for answers, then close the socket
+      Process.send_after(self(), :timeout, 6000)
+      {:noreply, %State{state |
+        timer_ref: Process.send_after(self(), :ok, @twenty_minutes)}}
+    else
+      send_msearch_message(state.socket)
+      {:noreply, %State{socket: state.socket,
+        attempts_left: state.attempts_left - 1,
+        timer_ref: Process.send_after(self(), :ok, 1000)}}
+    end
+  end
+
+  defp open_unicast_socket() do
+    :gen_udp.open(0, [{:reuseaddr, true}])
+  end
+
+  defp send_msearch_message(socket) do
+    message = build_search_msg("ssdp:all")
+    :gen_udp.send(socket, {239, 255, 255, 250}, 1900, message)
+  end
+
+  defp build_search_msg(search_target) do
+    "M-SEARCH * HTTP/1.1\r\n" <>
+      "Host: 239.255.255.250:1900\r\n" <>
+        "MAN: \"ssdp:discover\"\r\n" <>
+          "ST: #{search_target}\r\n" <>
+            "MX: 5\r\n\r\n"
+  end
+
+  def close_socket(state) do
+    unless is_nil(state.socket) do
+      :gen_udp.close(state.socket)
+    end
+  end
+end
--- lib/search/supervisor.ex
+++ lib/search/supervisor.ex
@@ -1,4 +1,4 @@
-defmodule Ssdp.Supervisor do
+defmodule Ssdp.Search.Supervisor do
 # SPDX-License-Identifier: Apache-2.0
 
   use Supervisor
@@ -10,11 +10,9 @@ defmodule Ssdp.Supervisor do
   @impl true
   def init(:ok) do
     children = [
-      {Ssdp.DeviceDb, name: Ssdp.DeviceDb},
-      Ssdp.MCast.Supervisor,
-      Ssdp.Search.Supervisor
+      Ssdp.Search.Broadcaster,
     ]
 
-    Supervisor.init(children, strategy: :rest_for_one)
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
