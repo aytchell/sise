fe0f012620afa4d9c5a5f7be32ef0fb71ba2ef18
Hannes Lerchl <hannes.lerchl@googlemail.com>
2021-04-04 17:30:44 +0200

    Added documentation

--- lib/sise/sise.ex
+++ lib/sise/sise.ex
@@ -30,16 +30,47 @@ defmodule Sise do
   """
   @type nt :: :all | String.t()
 
+  @doc """
+  Get discovered device and service information
+
+  Call this function to get information about discovered devices and services.
+  """
   @spec get(nt()) :: [Sise.Discovery.t()]
   def get(notification_type) do
     Sise.Cache.DeviceDb.get(notification_type)
   end
 
+  @doc """
+  Subscribe to receive notifications about discoveries
+
+  When calling this function, the caller will
+  - receive a notification message of type `:ssdp_add` for every already known
+    discovery
+  - receive a message for every change in the future
+
+  The possible notification messages to be received are these:
+  ```
+  {:ssdp_add, Sise.Discovery.t()}
+  {:ssdp_update, Sise.Discovery.t()}
+  {:ssdp_delete, Sise.Discovery.t()}
+  ```
+
+  It is possible to subscribe to multiple different notification types.
+  """
   @spec subscribe(nt()) :: nil
   def subscribe(notification_type) do
     Sise.Cache.DeviceDb.subscribe(notification_type)
   end
 
+  @doc """
+  Unsubribe from notifications about discoveries.
+  
+  Note that the notification mechanism does a very simple matching for
+  notification types. If you subscribe to multiple concrete services/devices
+  and then `unsubscribe(:all)`, they will all be removed.
+  However if you subscribe to `:all` services/device and then unsubscribe from 
+  a concrete one you will still get all notifications.
+  """
   @spec unsubscribe(nt()) :: nil
   def unsubscribe(notification_type) do
     Sise.Cache.DeviceDb.unsubscribe(notification_type)
